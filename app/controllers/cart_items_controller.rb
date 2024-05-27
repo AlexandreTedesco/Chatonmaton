49216e55f68a16fef36cb221017e2209d29e4815
paupiety <manonesqu@gmail.com>
2024-05-27 14:56:54 +0200

    creation method for cart and cart_item + migration devise_create_users

---
+++ app/controllers/cart_items_controller.rb
@@ -0,0 +1,22 @@
+class CartItemsController < ApplicationController
+    def create()
+        id = params[:id]
+        @cart = Cart.find_by(user: current_user)
+        @item = Item.find(id)
+        @cart_item = CartItem.new(cart: @cart, item: @item)
+        if @cart_item.save
+            redirect_back(fallback_location: root_path)
+           flash[:success] = "L'article a bien été ajouté au panier" 
+        end
+    end
+
+    def destroy()
+        id = params[:id]
+        @cart = Cart.find_by(user: current_user)
+        @item = Item.find(id)
+        @cart_item = CartItem.find_by(cart: @cart, item: @item)
+        @cart_item.destroy
+        redirect_back(fallback_location: root_path)
+        flash[:success] = "L'article a bien été supprimé de votre panier"
+    end
+end
