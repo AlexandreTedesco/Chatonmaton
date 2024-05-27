f39214b79d64f56613e54b55f2dc8d3749adcade
paupiety <manonesqu@gmail.com>
2024-05-27 12:37:44 +0200

    add model cart_item and complete element on cart

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,2 +1,5 @@
 class Cart < ApplicationRecord
+    belongs to :user
+    has_many :cart_items
+    has_many :items, through: :cart_items
 end
--- db/migrate/20240527095256_create_carts.rb
+++ db/migrate/20240527095256_create_carts.rb
@@ -1,7 +1,7 @@
 class CreateCarts < ActiveRecord::Migration[7.1]
   def change
     create_table :carts do |t|
-
+      t.belongs to :user
       t.timestamps
     end
   end
