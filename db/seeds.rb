d0c1164effe27eb9f77569c6ce19f34c5fdc742e
root <root@TL>
2024-05-27 17:48:36 +0200

    profile

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,3 +21,13 @@ ActiveRecord::Base.connection.reset_pk_sequence!('items')
     image_url: Faker::LoremFlickr.image(size: "300x300", search_terms: ['kitten'])
   )
 end
+
+10.times do
+  User.create!(
+    email: Faker::Internet.email,
+    password: Faker::Internet.password(min_length: 8),
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    description: Faker::Lorem.paragraph(sentence_count: 3)
+  )
+end
