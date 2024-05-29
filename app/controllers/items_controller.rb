0f6a756fa63f5c53b7772cef6922bc9ea870b412
AlexandreTedesco <alexandre.tedesco75@gmail.com>
2024-05-29 10:39:44 +0200

    ajouter une image

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -44,6 +44,6 @@ class ItemsController < ApplicationController
   end
 
   def item_params
-    params.require(:item).permit(:title, :description, :price, :image_url)
+    params.require(:item).permit(:title, :description, :price, :image)
   end
 end
