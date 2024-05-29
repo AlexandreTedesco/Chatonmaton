336574ba81fa654ce9688002934940eb60d0b24b
ThibaultL24 <thibault.lenormand24@gmail.com>
2024-05-29 13:16:33 +0200

    home_page+order

--- app/controllers/checkout_controller.rb
+++ app/controllers/checkout_controller.rb
@@ -3,7 +3,7 @@
 class CheckoutController < ApplicationController
   def create
     @total = params[:total].to_d
-    @event_id = params[:cart_id]
+    @cart_id = params[:cart_id]
     @session = Stripe::Checkout::Session.create(
       payment_method_types: ['card'],
       line_items: [
@@ -19,7 +19,7 @@ class CheckoutController < ApplicationController
         }
       ],
       metadata: {
-        event_id: @event_id
+        cart_id: @cart_id
       },
       mode: 'payment',
       success_url: "#{checkout_success_url}?session_id={CHECKOUT_SESSION_ID}",
@@ -31,16 +31,18 @@ class CheckoutController < ApplicationController
   def success
     @session = Stripe::Checkout::Session.retrieve(params[:session_id])
     @payment_intent = Stripe::PaymentIntent.retrieve(@session.payment_intent)
-    @event_id = @session.metadata.event_id
+    @cart_id = @session.metadata.cart_id
 
-    # Créer une instance de Attendance pour enregistrer la participation à l'événement
-    Attendance.create(user_id: current_user.id, event_id: @event_id)
+    # Enregistrer la commande dans la base de données
+    Order.create(
+      user_id: current_user.id,
+      cart_id: @cart_id,
+      stripe_session_id: @session.id
+    )
   end
 
   def cancel
     flash[:alert] = 'Le paiement a été annulé ou a échoué.'
-
-    # Rediriger l'utilisateur vers une page appropriée ou à l'endroit approprié de votre application
     redirect_to root_path
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_29_075812) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_29_104519) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -63,6 +63,13 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_29_075812) do
     t.text "description"
     t.decimal "price"
     t.string "image_url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.integer "user_id"
+    t.string "cart_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
